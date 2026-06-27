c411f31308901017a1e1a1db05129d64f3e4a16e
Aaro Koinsaari <89689072+koinsaari@users.noreply.github.com>
2026-06-27 12:15:18 +0300

    chore: cleanup workflows

--- bin/diagnostics.sh
+++ bin/diagnostics.sh
@@ -15,10 +15,3 @@ docker compose -f /srv/stoganet/compose/docker-compose.yml ps
 echo '```'
 echo
 
-for svc in traefik jellyfin qbittorrent sonarr radarr; do
-  echo "### docker logs --tail 20 $svc"
-  echo '```'
-  docker logs --tail 20 "$svc" 2>&1
-  echo '```'
-  echo
-done
